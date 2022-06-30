b36e105087955fc2335d021543fe0ad460148f08
nici200000 <83073771+nici200000@users.noreply.github.com>
2022-06-30 12:07:22 +0200

    Update header.php

--- header.php
+++ header.php
@@ -5,9 +5,7 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="profile" href="https://gmpg.org/xfn/11">
-        <title>
-          Zentile - Your Open Source Project
-        </title>
+
         <?php wp_head(); ?>
     </head>
 
